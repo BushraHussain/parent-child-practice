752cfb89cb39494db0aa81bca039ec91994a15c9
Bushra Hussain <BushraHussain@users.noreply.github.com>
2020-06-11 22:14:34 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+import Toyota from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Toyota />
   </React.StrictMode>,
   document.getElementById('root')
 );
